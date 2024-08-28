0e7d6c3a28e42443a4acb528cd25233f69a7f66c
Bautista Centorbi <bautii.centorbii@gmail.com>
2024-08-28 13:28:40 -0300

    SonnerToast on success cart

--- app/components/ui/Item/ItemDetail.jsx
+++ app/components/ui/Item/ItemDetail.jsx
@@ -1,14 +1,18 @@
 import React, { useState } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
+import { Toaster, toast } from 'sonner'
 import StockCounter from '../../Counter/StockCounter'
 import useCostTransform from '@/app/hooks/useCostTransform'
 import { useCartContext } from '@/app/Context/CartContext'
+import { useRouter } from 'next/navigation'
+import { redirect } from 'next/navigation'
 
 const ItemDetail = ({name, img, price,lgDescription, id, stock }) => {
     const [ cantidad, setCantidad ] = useState(0)
     const {costTransform} = useCostTransform()
     const {addToCart} = useCartContext()
+    const router = useRouter()
 
     const onAdd = (quantity) => {
         const item = {
@@ -19,11 +23,16 @@ const ItemDetail = ({name, img, price,lgDescription, id, stock }) => {
             stock
         }
         addToCart(item,quantity)
+        toast.success((quantity == 1) ? `Agregaste ${quantity} unidad` : `Agregaste ${quantity} unidades`, { action: {
+            label: 'Ver Producto',
+            onClick: () => router.push('/cart'),
+        },})
         setCantidad(quantity)
     }
 
     return (
     <article className='max-w-xs md:max-w-6xl bg-white border border-gray-200 rounded-2xl shadow dark:bg-background-dark dark:border-gray-900'>
+        <Toaster richColors expand={false} />
         <div className="flex flex-col md:flex-row">
             <div className='pt-8 md:pl-16 flex justify-center items-center'>
                 <Image 
